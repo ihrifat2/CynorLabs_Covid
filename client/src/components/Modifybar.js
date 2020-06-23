b138b02b3bc40eee349be3782e45b8371c25be2c
Imran Hadid <imranhadid03@gmail.com>
2020-06-23 19:42:25 +0600

    Create Modifybar.js

---
+++ client/src/components/Modifybar.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import { ReactFormGenerator, ElementStore } from 'react-form-builder2';
+import { Container, Row, Col } from 'reactstrap';
+import { put } from './requests';
+
+export default class Modifybar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [],
+            previewVisible: false,
+        };
+
+        const update = this._onChange.bind(this);
+        this._onSubmit = this._onSubmit.bind(this);
+        ElementStore.subscribe(state => update(state.data));
+    }
+
+    showPreview() {
+        this.setState({
+            previewVisible: true,
+        });
+    }
+
+    closePreview() {
+        this.setState({
+            previewVisible: false,
+        });
+    }
+
+    _onChange(data) {
+        this.setState({
+            data,
+        });
+    }
+
+    _onSubmit(data) {
+        console.log('onSubmit', data);
+        const id = this.props.id;
+        put(`/api/posts/${id}`, this.props.fromdata);
+        this.closePreview()
+    }
+
+    render() {
+        let modalClass = 'modal';
+        if (this.state.previewVisible) {
+            modalClass += ' show d-block';
+        }
+
+        return (
+            <Container>
+                <Row xs="1">
+                    <Col className="mt-4">
+                        <div className="clearfix" style={{ margin: '10px', width: '70%' }}>
+                            <h4 className="float-left">Preview</h4>
+                            <button className="btn btn-primary float-right" style={{ marginRight: '10px' }} onClick={this.showPreview.bind(this)}>Preview Form</button>
+                            {
+                                this.state.previewVisible &&
+                                <div className={modalClass} role="dialog">
+                                    <div className="modal-dialog" role="document">
+                                        <div className="modal-content"  style={{ padding: '30px' }}>
+                                            <Row xs="1">
+                                                <Col className="mt-4">
+                                                    <ReactFormGenerator
+                                                        download_path=""
+                                                        back_action="/manager"
+                                                        back_name="Back"
+                                                        answer_data={{}}
+                                                        action_name="Update"
+                                                        form_action="/"
+                                                        form_method="POST"
+                                                        onSubmit={this._onSubmit}
+                                                        variables={this.props.variables}
+                                                        data={this.state.data} />
+
+                                                    <div className="modal-footer">
+                                                        <button type="button" className="btn btn-default" data-dismiss="modal" onClick={this.closePreview.bind(this)}>Close</button>
+                                                    </div>
+                                                </Col>
+                                            </Row>
+                                        </div>
+                                    </div>
+                                </div>
+                            }
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        );
+    }
+}
