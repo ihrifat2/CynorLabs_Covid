b75c0f9740f5c6cc6b48b47aefd91e896351a176
Imran Hadid <imranhadid03@gmail.com>
2020-06-27 12:11:21 +0600

    Update ModifyForm.js

--- client/src/components/ModifyForm.js
+++ client/src/components/ModifyForm.js
@@ -5,6 +5,7 @@ import Modifybar from './Modifybar'
 import * as variables from './variables'
 import { getFormData } from './requests'
 import { Container, Row, Col } from 'reactstrap'
+import '../translator.css'
 
 function ModifyForm(props) {
     let { id } = useParams()
@@ -24,21 +25,88 @@ function ModifyForm(props) {
         setloading(false)
         return editableFormData
     }
-    
+
+    const items = [
+        {
+            key: 'Header',
+            name: 'Titre',
+            icon: 'fas fa-heading',
+            static: true,
+            content: 'Remplir le champs svp ...'
+        },
+        {
+            key: 'Label',
+            name: 'Label',
+            icon: 'fas fa-font',
+            static: true,
+            content: 'Placeholder Text...'
+        },
+        {
+            key: 'Paragraph',
+            name: 'Paragraph',
+            static: true,
+            icon: 'fa fa-paragraph',
+            content: 'Remplir le champs svp ...'
+        },
+        {
+            key: 'LineBreak',
+            name: 'LineBreak',
+            static: true,
+            icon: 'fas fa-arrows-alt-h',
+            content: 'Line Break'
+        },
+        {
+            key: 'Dropdown',
+            canHaveAnswer: true,
+            name: 'Dropdown',
+            icon: 'far fa-caret-square-down',
+            label: 'Placeholder Label',
+            field_name: 'dropdown_',
+            options: [],
+        },
+        {
+            key: 'Tags',
+            name: 'Tags',
+            static: true,
+            icon: 'fas fa-tags',
+            content: 'Placeholder Label',
+            options: [],
+        },
+        {
+            key: 'TextInput',
+            canHaveAnswer: true,
+            canHaveAlternateForm: false,
+            name: 'Text Input',
+            label: 'Placeholder Label',
+            icon: 'fas fa-font',
+            field_name: 'text_input_',
+        },
+        {
+            key: 'RadioButtons',
+            canHaveOptionValue: false,
+            name: 'Multiple Choice',
+            icon: 'fas fa-dot-circle',
+            label: 'Placeholder Label',
+            field_name: 'radiobuttons_',
+            options: [],
+        },
+    ];
+
     return (
         <Container>
             <Row xs="1">
                 <Col className="mt-4">
                     {
                         loading ?
-                        "Loading..." :
-                        <div>
-                            <Modifybar variables={variables} fromdata={data} id={id} />
-                            <ReactFormBuilder
-                                onPost={onPost}
-                                onLoad={onLoad}
-                            />
-                        </div>
+                            "Loading ..." :
+                            <div>
+                                <Modifybar variables={variables} fromdata={data} id={id} />
+                                <ReactFormBuilder
+                                    toolbarItems={items}
+                                    onPost={onPost}
+                                    onLoad={onLoad}
+                                />
+                            </div>
                     }
                 </Col>
             </Row>
