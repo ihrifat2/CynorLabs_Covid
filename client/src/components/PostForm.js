2c89b2c615409389b8b18a5916ff87f66c865b8f
Imran Hadid <imranhadid03@gmail.com>
2020-06-27 10:27:39 +0600

    Update PostForm.js

--- client/src/components/PostForm.js
+++ client/src/components/PostForm.js
@@ -11,10 +11,55 @@ function PostForm(props) {
         setdata(data)
     };
 
+    const items = [
+        {
+            key: 'Header',
+            name: 'Titre',
+            icon: 'fas fa-heading',
+            static: true,
+            content: 'Remplir le champs svp ...'
+        },
+        {
+            key: 'Paragraph',
+            name: 'Paragraph',
+            static: true,
+            icon: 'fa fa-paragraph',
+            content: 'Remplir le champs svp ...'
+        },
+        {
+            key: 'TextInput',
+            canHaveAnswer: true,
+            canHaveAlternateForm: false,
+            name: 'Text Input',
+            label: 'Placeholder Label',
+            icon: 'fas fa-font',
+            field_name: 'text_input_',
+        },
+        {
+            key: 'Dropdown',
+            canHaveAnswer: true,
+            name: 'Dropdown',
+            icon: 'far fa-caret-square-down',
+            label: 'Placeholder Label',
+            field_name: 'dropdown_',
+            options: [],
+        },
+        {
+            key: 'RadioButtons',
+            canHaveOptionValue: false,
+            name: 'Multiple Choice',
+            icon: 'fas fa-dot-circle',
+            label: 'Placeholder Label',
+            field_name: 'radiobuttons_',
+            options: [],
+        },
+    ];
+
     return (
         <div>
             <Headerbar variables={variables} fromdata={data} />
             <ReactFormBuilder
+                toolbarItems={items}
                 onPost={onPost}
             />
         </div>
